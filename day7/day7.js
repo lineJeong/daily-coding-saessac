4aa9ed57f3b2d6af681e61d3687c737149833ba2
lineJeong <line.jeong32@gmail.com>
2022-06-12 00:15:12 +0900

    daily-coding-saessac7

---
+++ day7/day7.js
@@ -0,0 +1,8 @@
+// 문제7 : 변수명
+// 다음 중 변수명으로 사용할 수 없는 것 2개를 고르시오.
+
+// 1) age
+// 2) &age 🔥 SyntaxError: Invalid left-hand side in assignment
+// 3) let 🔥 Uncaught SyntaxError: let is disallowed as a lexically bound name
+// 4) _age
+// 5) 1age
