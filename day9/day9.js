832ba04dd47d987336e3b7d79d799701c6e4b6ee
lineJeong <line.jeong32@gmail.com>
2022-06-14 00:51:49 +0900

    daily-coding-saessac9

---
+++ day9/day9.js
@@ -0,0 +1,16 @@
+// 문제9 : concat을 활용한 출력 방법
+// 다음 소스 코드를 완성하여 날짜와 시간을 출력하시오.
+
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second); // 🔥
+
+console.log(result);
+
+// 출력
+// 2019/04/24 11:34:27
