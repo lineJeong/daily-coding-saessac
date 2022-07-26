cab8ab462f99f8991877229f246cde573d86c07a
lineJeong <line.jeong32@gmail.com>
2022-07-26 21:32:40 +0900

    daily-coding-saessac50 수정

--- day50/day50.js
+++ day50/day50.js
@@ -8,7 +8,7 @@ function bubble(arr) {
   let result = arr.slice();
 
   for (let i = 0; i < result.length - 1; i++) {
-    for (let j = 0; j < result.length - i; i++) {
+    for (let j = 0; j < result.length - i; j++) {
       if (result[j] > result[j + 1]) {
         let temp = result[j];
         result[j] = result[j + 1];
